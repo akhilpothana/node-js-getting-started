11ff8ed6fec706de71638629bee3fbec4f4007fc
Akhil Pothana <akhilpothana@yahoo.com>
2021-12-18 22:38:02 -0500

    fix routes

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/static', express.static('public'))
+  .use(express.static('public'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/.well-known/apple-app-site-association', (req, res) => res.send('testing123'))
-  .get('/.well-known/apple-app-site-association1', (req, res) => res.sendFile('apple-app-site-assocation'))
-  .get('/.well-known/apple-app-site-association2', (req, res) => res.sendFile('Pic.png'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
