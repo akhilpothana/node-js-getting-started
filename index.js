d11d4c65943a8c9ecf11f99ba857667a7decdedf
Akhil Pothana <akhilpothana@yahoo.com>
2021-12-18 22:31:09 -0500

    experimenting with serving files

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/.well-known/apple-app-site-association', (req, res) => res.send('testing123'))
+  .get('/.well-known/apple-app-site-association1', (req, res) => res.sendFile('apple-app-site-assocation'))
+  .get('/.well-known/apple-app-site-association2', (req, res) => res.sendFile('Pic.png'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
