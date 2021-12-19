921998e2c0fb64eecfa7ea2443003f2a075ab5c0
Akhil Pothana <akhilpothana@yahoo.com>
2021-12-18 22:56:11 -0500

    working on routes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 5000;
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.static('public'))
+  .use(express.static('public/.well-known'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/.well-known/apple-app-site-association', (req, res) => res.sendFile('apple-app-site-association'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
