d6d1fbcd453276808fab9d2f1ab2e218a8598650
Akhil Pothana <akhilpothana@yahoo.com>
2021-12-19 00:15:08 -0500

    trying something with serving aasa file

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const aasaString = 
+`
+{
+  "applinks": {
+      "details": [
+           {
+             "appIDs": [ "1601071125.akhil.Money-Data" ]
+           }
+       ]
+   }
+}
+`
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.static('public'))
-  .use(express.static('public/.well-known'))
+  //.use(express.static('public/.well-known'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/.well-known/apple-app-site-association', (req, res) => res.send(aasaString))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
